201fb87f28a7d0db1a890fac13bc579af2913340
Zainab Al-Najjar <eng.zainab.alnajar@gmail.com>
2024-02-26 16:25:19 +0300

    Completed assignment!

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(function(driver){
+        return driver.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(drivers,name){
+    return drivers.filter(function(driver){
+        return driver.startsWith(name);
+    })
+}
+function matchName(drivers,name){
+    return drivers.filter(function(driver){
+        return driver.name === name;
+    })
+}
+// Test:
+// const drivers = [];
+// drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
+// console.log(fuzzyMatch(drivers,"Sa"))
